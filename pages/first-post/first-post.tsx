1b7583a2f087c3e97b99bc0e32420887ff6a0340
maxunbearable <hypnofroghs@gmail.com>
2023-09-06 21:20:02 +0300

    c

--- pages/first-post/first-post.tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function FirstPost() {
-    const router = useRouter();
-    const { slug } = router.query;
-
-    return <div>Post: { slug }</div>;
-}
